ed89c01f0f58a107dff7c6021a1509580f23b38d
Radhitya Putra <radhityakomang@gmail.com>
2026-05-20 11:18:10 +0800

    update user

--- src/main/java/com/mycompany/monitoringenergirumah/Model/User.java
+++ src/main/java/com/mycompany/monitoringenergirumah/Model/User.java
@@ -9,5 +9,50 @@ package com.mycompany.monitoringenergirumah.Model;
  * @author ASUS
  */
 public class User {
+    private static int idCounter = 1;
     
+    private final int id;
+    private String namaLengkap;
+    private String email;
+    private String password;
+    
+    public User(String namaLengkap, String email, String password) {
+        this.id          = idCounter++;
+        this.namaLengkap = namaLengkap;
+        this.email       = email;
+        this.password    = password;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+    
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+     @Override
+    public String toString() {
+        return "User{id=" + id + ", nama='" + namaLengkap + "', email='" + email + "'}";
+    }
 }
